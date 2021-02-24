7b96d683023c9e9c9dd7313cf926bd53c3c7db8c
Nikita Zhigulin <nikita1number@gmail.com>
2021-02-24 11:18:09 +0300

    upd

--- Course/Program.cs
+++ Course/Program.cs
@@ -3,125 +3,149 @@ using System.Collections.Generic;
 
 namespace ConsoleApp10
 {
-    public class Student
+    public class Product
     {
-        public string FirstName;
-        public string LastName;
-        public int Age;
+        public string Name;
+        public decimal Price;
 
-        public Student(string firstName, string lastName, int age)
+        public Product(string name, decimal price)
         {
-            FirstName = firstName;
-            LastName = lastName;
-            Age = age;
+            Name = name;
+            Price = price;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name} {Price}");
         }
     }
 
-    public class School
+    public class Order
     {
-        public string Name;
-        public List<Student> Students;
+        public List<Product> Products;
+        public decimal FullPrice;
 
-        public School(string name)
+        public Order(List<Product> products)
         {
-            Name = name;
-            Students = new List<Student>();
+            Products = products;
+
+            FullPrice = 0;
+
+            foreach (var product in products)
+            {
+                FullPrice += product.Price;
+            }
+        }
+    }
+
+    public class Store
+    {
+        public List<Product> Products;
+        public List<Product> Basket;
+        public List<Order> Orders;
+
+        public Store()
+        {
+            Products = new List<Product>
+            {
+            new Product("Хлеб", 25),
+            new Product("Молоко", 50),
+            new Product("Мороженное", 45),
+            new Product("Сок", 100),
+            new Product("Яблоко", 15),
+            new Product("Масло", 30)
+            };
+
+            Basket = new List<Product>();
+            Orders = new List<Order>();
         }
 
-        public void PrintStudents()
+        public void ShowCatalog()
         {
-            if (Students.Count == 0)
-                Console.WriteLine($"В школе {Name} нет учеников");
-            else
+            Console.WriteLine("Каталог продуктов: ");
+            ShowProducts(Products);
+        }
+
+        public void ShowBasket()
+        {
+            Console.WriteLine("Корзина продуктов: ");
+            ShowProducts(Basket);
+        }
+
+
+        public void ShowProducts(List<Product> products)
+        {
+            for (int i = 0; i < products.Count; i++)
             {
-                Console.WriteLine("{0, -2} {1, -10} {2, -10} {3, -2}", "ID", "Имя", "Фамилия", "Возраст");
-                for (int i = 0; i < Students.Count; i++)
-                {
-                    Console.WriteLine("{0, -2} {1, -10} {2, -10} {3, -2}", i + ".", Students[i].FirstName, Students[i].LastName, Students[i].Age);
-                }
+                Console.WriteLine($"{i}. {products[i].Name} {products[i].Price}");
             }
         }
 
-        public void AddNewStudent(Student student)
+        public void AddToBasket(int numberProduct)
         {
-            Students.Add(student);
-            Console.WriteLine($"Студент {student.FirstName} {student.LastName} успешно добавлен в школу {Name}.");
+            Basket.Add(Products[numberProduct]);
+            Console.WriteLine($"{Products[numberProduct].Name} успешно добавлен в корзину.");
+            Console.WriteLine($"В корзине {Basket.Count} продуктов.");
         }
 
-        internal void RemoveStudent(int choiseUser)
+        public void CreateOrder()
         {
-            Console.WriteLine($"Студент {Students[choiseUser].FirstName} {Students[choiseUser].LastName} был удален.");
-            Students.RemoveAt(choiseUser);
+            Order order = new Order(Basket);
+            Orders.Add(order);
+            Basket.Clear();
+            Console.WriteLine($"К оплате {order.FullPrice}.");
         }
     }
+        
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Здравствуйте! Введите название вашей школы");
-            string schoolName = Console.ReadLine();
-            School school = new School(schoolName);
-            Console.WriteLine($"Школа {school.Name} успешна создана");
+
+            Store onlineStore = new Store();
+
+            Console.WriteLine("Здравствуйте, выберите действие:");
 
             while(true)
             {
-                Console.WriteLine("Выберите действие: ");
-                Console.WriteLine($"1. Просмотр списка учеников школы {school.Name}. ");
-                Console.WriteLine($"2. Добавление нового ученика в список учащихся школы {school.Name}.");
-                Console.WriteLine($"3. Удаление ученика из списка учащихся школы {school.Name}.");
-                int choiseUser = int.Parse(Console.ReadLine());
-
-                if(choiseUser != 1 && choiseUser != 2 && choiseUser != 3)
+                Console.WriteLine("______________________________________________");
+                Console.WriteLine("1. Показать каталог продуктов.");
+                Console.WriteLine("2. Добавление продукта в коризну.");
+                Console.WriteLine("3. Просмотр корзины.");
+                Console.WriteLine("4. Оформление заказа.");
+                Console.WriteLine("______________________________________________");
+                int numberAction = int.Parse(Console.ReadLine());
+                switch (numberAction)
                 {
-                    Console.WriteLine("Некорректный ввод символа! Напишите число от 1 до 3.");
-                    continue;
+                    case 1:
+                        onlineStore.ShowCatalog();
+                        break;
+                    case 2:
+                        onlineStore.ShowCatalog();
+                        Console.WriteLine("Напишите номер продукта, который нужно добавить в корзину. " +
+                            "\nЕсли хотите прекратить добавлять продукты в корзину напишите - -1.");
+                        int productNumber = 0;
+                        do
+                        {
+                            productNumber = Convert.ToInt32(Console.ReadLine());
+                            if (productNumber != -1)
+                            {
+                                onlineStore.AddToBasket(productNumber);
+                            }
+                        } while (productNumber != -1);
+                        break;
+                    case 3:
+                        onlineStore.ShowBasket();
+                        break;
+                    case 4:
+                        onlineStore.CreateOrder();
+                        break;
+                    default:
+                        Console.WriteLine("Вы ничего не выбрали, выберите номер действия");
+                        break;
                 }
-                else if(choiseUser == 1)
-                {
-                    school.PrintStudents();
-                }
-                else if(choiseUser == 2)
-                {
-                    Console.WriteLine("Введите имя ученика");
-                    string firstName = Console.ReadLine();
-                    Console.WriteLine("Введите фамилию ученика");
-                    string lastName = Console.ReadLine();
-                    bool isTrue;
-                    int age = 0;
-                    do
-                    {
-                        Console.WriteLine("Введите возраст ученика");
-                        isTrue = int.TryParse(Console.ReadLine(), out age);
-                        if (!isTrue)
-                            Console.WriteLine("Введите число!");
-                    }
-                    while (!isTrue);
-
-                    Student student = new Student(firstName, lastName, age);
-                    school.AddNewStudent(student);
-                }
-                else if(choiseUser == 3)
-                {
-                    school.PrintStudents();
-                    bool isTrue;
-
-                    do
-                    {
-                        Console.WriteLine("Выберите ID ученика, которого хотите удалить");
-                        isTrue = int.TryParse(Console.ReadLine(), out choiseUser);
-                        if (!isTrue || choiseUser >= school.Students.Count || choiseUser < 0)
-                            Console.WriteLine("Введите корректное число!");
-                        else
-                            school.RemoveStudent(choiseUser);
-                    }
-                    while (!isTrue);
-                    
-                }
-
             }
-
+            
         }
-
     }
-
 }
